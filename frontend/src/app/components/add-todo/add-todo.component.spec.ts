664681d8796c4b9977e84e6ebcda84c56e6cd2cd
TimoHubner444 <151617361+TimoHubner444@users.noreply.github.com>
2025-02-28 13:15:05 +0100

    Update add-todo.component.spec.ts

--- frontend/src/app/components/add-todo/add-todo.component.spec.ts
+++ frontend/src/app/components/add-todo/add-todo.component.spec.ts
@@ -1,21 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { CarrouselComponent } from './carrousel.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AddTodoComponent } from './add-todo.component';
 
-describe('CarrouselComponent', () => {
-  let component: CarrouselComponent;
-  let fixture: ComponentFixture<CarrouselComponent>;
+describe('AddTodoComponent', () => {
+  let component: AddTodoComponent;
+  let fixture: ComponentFixture<AddTodoComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      declarations: [ CarrouselComponent ]
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      declarations: [ AddTodoComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CarrouselComponent);
+    fixture = TestBed.createComponent(AddTodoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
